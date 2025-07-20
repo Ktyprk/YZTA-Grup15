38078cd453ed48e3bb901edfe90770ccfcb65411
Ktyprk <Kutayperk@hotmail.com>
2025-07-20 22:14:12 +0300

    update

---
+++ Assets/HappyLama/Scripts/Runtime/ExposedProperty.cs
@@ -0,0 +1,22 @@
+﻿namespace HappyLama
+{
+    [System.Serializable]
+    public class ExposedProperty
+    {
+        public enum PropertyType { String, Integer, Float, Boolean }
+
+        public string PropertyName = "New Property";
+        public PropertyType Type = PropertyType.Integer;
+        public string StringValue = "";
+        public int IntValue = 0;
+        public float FloatValue = 0f;
+        public bool BoolValue = false;
+
+        public object PropertyValue { get; set; }
+
+        public static ExposedProperty CreateInstance()
+        {
+            return new ExposedProperty();
+        }
+    }
+}
